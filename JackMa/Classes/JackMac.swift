3552d88aec1bd6980d8fc412bd41171f68bdb0c9
huangwu1111 <1243930473@qq.com>
2023-08-14 16:09:26 +0800

    jack ma
    新建项目

---
+++ JackMa/Classes/JackMac.swift
@@ -0,0 +1,20 @@
+//
+//  JackMa.swift
+//  JackMa
+//
+//  Created by whqpMac007 on 2023/8/14.
+//
+
+import Foundation
+
+public class JackMac {
+    
+    public init() {
+        
+    }
+    
+    public func des(name: String) {
+        print("my name is \(name)")
+    }
+    
+}
